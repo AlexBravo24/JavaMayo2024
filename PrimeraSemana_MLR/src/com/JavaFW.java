dcab9e3cda835a585e131fdd138741c4097a3d44
misaeldlr <misael.deleon@gmail.com>
2024-06-04 08:15:12 -0600

    Ejercicios Sem2 MLR

--- PrimeraSemana_MLR/src/com/JavaFW.java
+++ PrimeraSemana_MLR/src/com/JavaFW.java
@@ -6,33 +6,31 @@ import java.io.FileWriter;
 public class JavaFW {
 
 	public static void main(String[] args) {
-		//uso de las clases Java File y File Writer
+		//Uso de las clases Java File y File Writer
 		
 		//Vamos a declarar una cadena de texto para
 		//escribirla en nuestro archivo de prueba
 		String texto = "\nMisael De Leon";
-
 		
 		try {
-			File archivo = new file ("C:\\Users\\hp\\Documents\\Curso\\Java\\fichero.txt");
+			File archivo = new File("C:\\Users\\hp\\Documents\\Curso\\Java\\fichero.txt");
 			
 			//Para escribir info en el archivo utilizamos la clase FileWriter
-			FileWriter linea  = new FileWriter(archivo, true);
-			//Nuestro objeto linea se contruye  con dos valores
-			//la ruta del archivo a sobre escribir y un valor boleano true
+			FileWriter linea = new FileWriter(archivo, true);
+			//Nuestro objeto linea se construye con dos valores,
+			//la ruta del archivo a sobre escribir y un valor booleano true
 			//para indicar que se respete el contenido original del archivo
 			//si no lo indicamos, el archivo se sobreescribe por completo
 			
-			//Indicamos la escritura del String texto en nuestro archivo
+			//indicamos la escritura del String texto en nuestro archivo
 			linea.write(texto);
-			//Necesitamos otra instruccion que guarde/confirme los cambios
+			//necesitamos otra instruccion que guarde/confirme los cambios
 			linea.close();
-			
-		} catch (Exeption e) {
+				
+		} catch (Exception e) {
 			System.out.println("No encontre el archivo");
 			e.printStackTrace();
 		}
-		
 
 	}
 
